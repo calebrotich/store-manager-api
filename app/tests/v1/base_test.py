92903935e70714d58d9b6ce8b3992073ff83b528
Caleb Rotich <calebrotich10@gmail.com>
2018-10-16 11:31:10 +0300

    [Chore #161201973] Create a base test file
    The base test class file will be inherited by all the other test files

---
+++ app/tests/v1/base_test.py
@@ -0,0 +1,44 @@
+"""
+    Contains the base test class for the
+    other test classes
+"""
+import unittest
+
+# local imports
+from app import create_app
+from instance.config import config
+
+
+class TestBaseClass(unittest.TestCase):
+    """Base test class"""
+
+
+    def setUp(self):
+        """Create and setup the application
+
+        for testing purposes
+        """
+        self.app = create_app('testing')
+        self.BASE_URL = 'api/v1'
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.app_test_client = self.app.test_client()
+        self.app.testing = True
+
+        self.PRODUCT = {
+        'product_name': 'Phone Model 1',
+        'product_price': 55000,
+        'category': 'Phones'
+        }
+
+
+    def tearDown(self):
+        """Destroy the application that
+
+        is created for testing
+        """
+        self.app_context.pop()
+
+
+if __name__ == '__main__':
+    unittest.main()

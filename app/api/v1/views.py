8db3b4381483ebd8b869d7698da2d5622bebd1a1
Caleb Rotich <calebrotich10@gmail.com>
2018-10-16 13:30:48 +0300

    [Feature #161201833] Create route for POST /products

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,12 +1,9 @@
-"""
-  Define API endpoints as routes
-"""
+"""Define API endpoints as routes"""
+
 from flask_restful import Api, Resource
 
-
-# local imports
 from . import v1_blueprint
-from .resources import endpoints
-
+from .resources import admin_endpoints
 
 API = Api(v1_blueprint)
+API.add_resource(admin_endpoints.Product, '/products')

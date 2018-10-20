f59d144bcd54006f7447da8f47d20ad401910147
Caleb Rotich <calebrotich10@gmail.com>
2018-10-20 19:42:50 +0300

    [Chore #161248446] Create users list

---
+++ app/api/v1/models/users.py
@@ -0,0 +1,22 @@
+from flask import make_response, jsonify
+USERS = []
+
+
+class User_Model():
+    def __init__(self, email, password, role):
+        self.id = len(USERS) + 1
+        self.email = email
+        self.password = password
+        self.role = role
+
+    def save(self):
+        new_user = {
+            "id": self.id,
+            "email": self.email,
+            "password": self.password,
+            "role": self.role
+            }
+            
+        USERS.append(new_user)
+        response = jsonify(new_user)
+        return response

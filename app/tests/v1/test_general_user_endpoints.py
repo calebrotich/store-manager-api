c6a0ee0932b90e47fbb5d851c1951233b05fdbe1
Caleb Rotich <calebrotich10@gmail.com>
2018-10-23 10:50:22 +0300

    [Chore #161201973] Refactor tests
    Modify tests to comply with the change in token decode and encode. Add a test case to increase coverage.

--- app/tests/v1/test_general_user_endpoints.py
+++ app/tests/v1/test_general_user_endpoints.py
@@ -18,8 +18,14 @@ class TestGeneralUsersEndpoints(base_test.TestBaseClass):
     def test_retrieve_all_products(self):
         """Test GET /products - when products exist"""
 
+        self.register_test_admin_account()
+        token = self.login_test_admin()
+
         response = self.app_test_client.get(
-            '{}/products'.format(self.BASE_URL))
+            '{}/products'.format(self.BASE_URL),
+            headers=dict(Authorization=token),
+            content_type='application/json'
+            )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(helper_functions.convert_response_to_json(
@@ -27,13 +33,19 @@ class TestGeneralUsersEndpoints(base_test.TestBaseClass):
 
     def test_retrieve_specific_product(self):
         """Test GET /products/id - when product exist"""
-            
+
+        self.register_test_admin_account()
+        token = self.login_test_admin()
+
         response = self.app_test_client.get(
-            '{}/products/1'.format(self.BASE_URL))
+            '{}/products/1'.format(self.BASE_URL),
+            headers=dict(Authorization=token),
+            content_type='application/json'
+            )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(helper_functions.convert_response_to_json(
-            response)['product_name'], self.PRODUCT['product_name'])
+            response)['product'], self.PRODUCT['product_name'])
 
     def test_retrieve_specific_sale_order(self):
         """Test GET /saleorder/id - when saleorder exists"""
@@ -49,13 +61,47 @@ class TestGeneralUsersEndpoints(base_test.TestBaseClass):
             'quantity': 1,
             'amount': 20
         },
-        headers=dict(Authorization="Bearer " + token),
+        headers=dict(Authorization=token),
         content_type='application/json')
 
         response = self.app_test_client.get(
             '{}/saleorder/1'.format(self.BASE_URL),
-            headers=dict(Authorization="Bearer " + token),
+            headers=dict(Authorization=token),
             content_type='application/json'
             )
 
         self.assertEqual(response.status_code, 200)
+
+
+    def test_missing_token(self):
+        """Test GET /products - when token is missing"""
+
+        self.register_test_admin_account()
+        token = ""
+
+        response = self.app_test_client.get(
+            '{}/products'.format(self.BASE_URL),
+            headers=dict(Authorization=token),
+            content_type='application/json'
+            )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(helper_functions.convert_response_to_json(
+            response)["Message"], "You need to login")
+
+
+    def test_invalid_token(self):
+        """Test GET /products - when token is missing"""
+
+        self.register_test_admin_account()
+        token = "sample_invalid-token-afskdghkfhwkedaf-ksfakjfwey"
+
+        response = self.app_test_client.get(
+            '{}/products'.format(self.BASE_URL),
+            headers=dict(Authorization=token),
+            content_type='application/json'
+            )
+
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(helper_functions.convert_response_to_json(
+            response)["Message"], "This token is invalid")
